a94fbfe7062968bc243a6ddfb04772e8f17d13ad
wawalid <128592343+wawalid@users.noreply.github.com>
2024-03-06 03:13:48 +0100

    Update login.py

--- codigo/login.py
+++ codigo/login.py
@@ -5,71 +5,55 @@ repetir = True
 
 
 def menu_admin():
+    repetir = True
+    print("\nBienvenido al panel de administrador")
     while repetir:
-        print("Bienvenido al panel de administrador")
-        print("A- Crear usuario \nB- Borrar usuario \nC- Consultar usuarios \nD- Salir")
+        print("\nA- Crear usuario \nB- Borrar usuario \nC- Modificar usuario \nD- Consultar usuarios \nF- Salir")
         opcion_1_1 = input("¿Que desea hacer?: ")
         if opcion_1_1.lower() == "a":
-            crear_usuarios()
+            with conex_bd() as conexion:
+                crear_usuarios(conexion)
         elif opcion_1_1.lower() == "b":
-            borrar_usuarios()
+            with conex_bd() as conexion:    
+                borrar_usuarios(conexion)
         elif opcion_1_1.lower() == "c":
-            consulta_user()
+            with conex_bd() as conexion:    
+                modificar_usuario(conexion)
         elif opcion_1_1.lower() == "d":
+            with conex_bd() as conexion:
+                mysql_lista = consultar_usuarios(conexion)
+                
+                if mysql_lista is not None:
+                    for usuario in mysql_lista:
+                        print(usuario)
+                else:
+                    print("Error fetching user data.")
+        elif opcion_1_1.lower() == "f":
             repetir = False
             print("Hasta la proxima")
         else:
-            print("Ópcion no válida")
+            print("Opción no válida")
 
 
-
+conexion = conex_bd()
 
 
 def menu_cliente():
-    print("menu cliente")
+    repetir = True
+    print(f"Bienvenido cliente con dni '{dni_user}' ")
+    while repetir:
+        print("A- Crear transacción \nB- Mostrar transacciones ")
 
 
 
 
 
-def consulta_user(usuario, contraseña):
-    host = 'localhost'
-    port = '3307'
-    dataBase = 'cajero'
-    user = 'root'
-    pss = 'walid00'
 
-    try:
-        conexion = mysql.connector.connect(
-            host=host,
-            port=port,
-            database=dataBase,
-            user=user,
-            password=pss
-        )
-
-        if conexion.is_connected():
-            mysql_cursor = conexion.cursor(dictionary=True) 
-            miConsulta=f"""
-
-                        select * from Usuario where dni = "{usuario}" and contraseña = "{contraseña}";
-                        
-                        """
-            
-            mysql_cursor.execute(miConsulta)
-            
-            mysql_lista = mysql_cursor.fetchall()
-            conexion.close()
-
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-
-    return mysql_lista
 
 print("Iniciar sesion")
 dni_user = input("Introduzca dni: ")
 pass_user = input("Introduzca contraseña: ")
-mysql_lista = consulta_user(dni_user, pass_user)
+mysql_lista = autenticacion(conexion , dni_user, pass_user)
 
 if mysql_lista:
     for usuario in mysql_lista:
